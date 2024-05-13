93ac5c31bb0db475016900c2b21c3235522ab277
kanjitta <jittakan2539@gmail.com>
2024-05-13 14:29:56 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const Products = {
+  product_id,
+  product_name,
+  quantity_in_stock,
+  unit_price,
+  product_description: {
+    product_brand,
+    size_or_quantity,
+    taste_or_scent,
+    colour,
+    additional_description,
+  },
+};
+
+console.log(Products.product_id);
